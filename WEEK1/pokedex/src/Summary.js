a9133a9255146b6cf3460f5af52748f8ddc78306
Aleyna Güner <aleynaguner@hotmail.com>
2020-11-04 16:33:01 +0300

    pokedex

--- WEEK1/pokedex/src/Summary.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Pokemon from "./Pokemon";
-
-class Summary extends React.Component {
-    render(){
-        return <Pokemon {...this.props} isSummary={true}></Pokemon>
-    }
-}
-export default Summary;
