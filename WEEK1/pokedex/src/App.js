bd5d5ece45683cbd4abcfc47045da2ad3ad9e670
Aleyna Güner <aleynaguner@hotmail.com>
2020-11-05 16:20:55 +0300

    pokedex

---
+++ WEEK1/pokedex/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+import pokemons from "./data";
+import Summary from "./Summary";
+
+
+class App extends  React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedPokemon: false
+        };
+
+        this.onPokemonClick = this.onPokemonClick.bind(this);
+    }
+
+    onPokemonClick(id){
+        const selectedPokemon = pokemons.find((pokemon) => {
+            return pokemon.id === id;
+        });
+        this.setState({
+            selectedPokemon
+        });
+    }
+
+    render(){
+        return (
+            <div className="App">
+                {this.state.selectedPokemon && <Summary {...this.state.selectedPokemon}/>}
+                {
+                    pokemons.map((item) => {
+                        return <Pokemon {...item} onPokemonClick={this.onPokemonClick} key={item.id}/>
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default App;
